3d2cd89b1d21ba7b359ebf4b4f035d9d9ac87444
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-04-15 13:56:03 -0400

    commit tree b218b0b9400568954705c3d388aee376ebcf2cad

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,16 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const directorInfo = directors.map(director => {
+    return <div><h3>{director.name}</h3>
+    <ul>
+      {director.movies.map(movie => <li key={movie}>{movie}</li>)}</ul></div>
+  })
+
+  return (<div>
+  <h1>Directors Page</h1>{directorInfo}</div>
+  )
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,22 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const movieDisplay = movies.map(movie=>{
+    return <div> <h3>{movie.title}</h3> 
+                <p>{movie.time} mins</p> 
+                  <ul>
+                  {movie.genres.map((genre) => (
+                     <li key={genre}>{genre}</li>
+                     ))}
+                  </ul>
+            </div>
+  })
+  return (
+
+    <div>
+      <h1>Movies Page</h1>{movieDisplay}</div>
+    )
 }
 
 export default Movies;
